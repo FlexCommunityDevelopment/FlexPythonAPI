078aedd6df4f5e7c8335fae2d4556343cbd1df6c
ldaponte <Foos.Ball1>
2015-02-23 19:06:13 +0100

    Initial check-in

---
+++ API.py
@@ -0,0 +1,188 @@
+import clr
+import binascii
+import time
+clr.AddReference("FlexLib")
+clr.AddReference("Flex.UiWpfFramework")
+clr.AddReference("System.Reflection")
+from CLR.System import Type
+from Flex.UiWpfFramework.Mvvm import ObservableObject
+from Flex.Smoothlake.FlexLib import IQStream
+from Flex.Smoothlake.FlexLib import API
+from Flex.Smoothlake.FlexLib import Panadapter
+from TextOutputWindow import TextOutputWindow
+from System.Windows import Size
+from System.Reflection import BindingFlags
+ 
+# ----------------------------------------
+# ----- API Event Handlers ---------------
+# ----------------------------------------
+
+rig = None
+pa = None
+iq = None
+fall = None
+maxPans = None
+iqReceived = 0
+panReceived = 0
+fallReceived = 0
+
+def RadioAdded(radio):
+    global rig
+    global maxPans
+    
+    rig = radio
+
+    msg_win.add('Radio Added: ' + radio.IP.ToString())
+
+    rig.MessageReceived += MessageReceived
+    rig.PropertyChanged += radio_PropertyChanged
+    rig.IQStreamAdded += IQStreamAdded
+    rig.PanadapterAdded += PanadapterAdded
+    rig.SliceAdded += SliceAdded
+ 
+    if rig.Model == 'FLEX-6300':
+        maxPans = 2
+    elif rig.Model == 'FLEX-6500':
+        maxPans = 4
+    elif rig.Model == 'FLEX-6700':
+        maxPans = 8
+    else:
+        maxPans = 2
+  
+    msg_win.add('Radio Model: ' + rig.Model)
+    msg_win.add('Max panadapters: ' + str(maxPans))
+  
+def RadioRemoved(radio):
+    msg_win.add('Radio Removed: ' + radio.IP.ToString())
+
+# ----------------------------------------
+# ----- Radio Event Handlers -------------
+# ----------------------------------------
+
+def MessageReceived(severity, msg):
+    msg_win.add('Message: ' + msg)
+
+def SliceAdded(slice):
+    msg_win.add('Slice added: ' + str(slice.Index))
+    
+def PanadapterAdded(panadapter, waterfall):
+    global pa
+    global rig
+    global iq
+    global fall
+    
+    msg_win.add('Panadapter added: ' + str(panadapter.StreamID))  
+    
+    fall = waterfall
+    
+    fall.DataReady += fall_DataReady
+    
+    pa = panadapter;
+
+    pa.DataReady += pan_DataReady;
+    pa.PropertyChanged += pan_PropertyChanged;
+
+    pa.DAXIQChannel = 1
+    pa.Band = "40"
+    pa.CenterFreq = 7.204
+    pa.RXAnt = "ANT1"
+    pa.Size = Size(400.0, 100.0)
+    pa.RequestPanadapterFromRadio()
+    
+    iq = rig.CreateIQStream(1)
+    iq.Pan = pa
+    iq.DataReady += iq_DataReady
+    iq.RequestIQStreamFromRadio()                
+
+def pan_DataReady(panadapter, data):
+    global panReceived
+    
+    panReceived += 1
+    if panReceived % 100 == 0:
+        msg_win.add('Pan Stream Id: %s, Count: %s, length: %s' % (panadapter.StreamID, str(panReceived), str(data.Length)))
+
+def fall_DataReady(waterfall, tile):
+    global fallReceived
+    
+    fallReceived += 1
+    if fallReceived % 100 == 0:
+        msg_win.add('Fall Stream Id: %s, Count: %s, TimeCode: %s' % (waterfall.StreamID, fallReceived, str(tile.Timecode)))
+        
+def pan_PropertyChanged(sender, event):
+    val = sender.GetType().GetProperty(event.PropertyName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase).GetValue(sender, None);
+    #msg_win.add('PA Change: %s Value: %s' % (event.PropertyName,val))
+    
+def radio_PropertyChanged(radio, event):
+    global maxPans
+    val = radio.GetType().GetProperty(event.PropertyName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase).GetValue(radio, None);
+    #msg_win.add('Change: %s Value: %s Type: %s' % (event.PropertyName,val, type(val).__name__))
+        
+    if type(val) is unicode:
+        val = '%s' % val
+        val = val.strip(' \t\n\r\0')
+    
+    if event.PropertyName == 'Status' and val == 'Available':
+        msg_win.add('Connecting...')
+        radio.Connect()
+    
+    elif event.PropertyName == 'PanadaptersRemaining' and val == maxPans:
+        msg_win.add('Requesting panfall...')
+        radio.RequestPanafall()  
+        
+    elif event.PropertyName == 'Nickname':
+        msg_win.add('Radio Name: ' + val)
+    
+def IQStreamAdded(stream):
+    msg_win.add('IQ Stream Added')
+    
+# ----------------------------------------
+# ----- Panadapter Event Handlers --------
+# ----------------------------------------
+
+def iq_DataReady(stream, data):
+    global iqReceived
+    
+    iqReceived += 1
+    if iqReceived % 500 == 0:
+        msg_win.add('Stream Id: %s, Count: %s, length: %s, BytesPerSecond: %s' % (stream.StreamID, str(iqReceived), str(data.Length), str(stream.BytesPerSecFromRadio)))
+         
+# ----------------------------------------
+# ----- main -----------------------------
+# ----------------------------------------
+
+def Quit():
+    global pa
+    global iq
+    global rig
+    
+    print('quiting...')
+    iq.DataReady -= iq_DataReady
+    iq.Close()
+    iq = None
+    pa.DataReady -= pan_DataReady
+    pa.Close(True)
+    pa = None
+    
+    rig.PropertyChanged -= radio_PropertyChanged
+    rig.MessageReceived -= MessageReceived
+    rig.IQStreamAdded -= IQStreamAdded
+    rig.PanadapterAdded -= PanadapterAdded
+    rig.SliceAdded -= SliceAdded
+    rig.Disconnect()
+
+    import sys
+    sys.exit(0)
+    
+if __name__ == '__main__':
+
+    inval = raw_input('hit enter to start')
+    
+    msg_win = TextOutputWindow(Quit)
+    
+    msg_win.add('Initializing')
+     
+    API.ProgramName = "Python"
+    API.RadioAdded += RadioAdded
+    API.RadioRemoved += RadioRemoved
+    API.Init()
+    
---
+++ TextOutputWindow.py
@@ -0,0 +1,62 @@
+from Tkinter import *
+import ScrolledText
+import threading
+import Queue
+
+class TextOutputWindow(threading.Thread):
+
+    def ShutDown(self):
+        return self.shutDown
+
+    def ShutDown(self, value):
+        self.shutDown = value
+        
+    def __init__(self, func):
+        self.shutDown = func
+        self.quit = 0
+        self.clear = 0
+        self.queue = Queue.Queue()
+        threading.Thread.__init__(self)
+        self.root = Tk()
+        self.root.title("FlexRadio Output")
+        self.start()
+        self.root.after(100, self.periodicCall)
+
+    def run(self):
+        self.root.protocol("WM_DELETE_WINDOW", self.quit_cb)
+        self.text = ScrolledText.ScrolledText(self.root)
+        self.text.pack(expand=YES, fill=BOTH)
+        self.root.mainloop()
+
+    def periodicCall(self):
+        if self.quit:
+            self.root.quit()
+        else:
+            if self.clear:
+                self.clear = 0
+                self.text.delete('1.0', END)
+            while self.queue.qsize():
+                try:
+                    msg = self.queue.get()
+                    self.text.insert(END, msg)
+                    self.text.see(END)
+                except Queuy.Empty:
+                    pass
+            self.root.after(100, self.periodicCall)
+
+    def quit_cb(self):
+        
+        #if self.shutDown != None:
+        print('shutting down now...')
+        while self.queue.qsize():
+            try:
+                self.queue.get()
+            except Queuy.Empty:
+                pass
+                
+        self.shutDown()
+            
+        self.root.quit()
+
+    def add(self, message):
+        self.queue.put(message + '\n')
